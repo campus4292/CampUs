c48a40759bd1d0fffad1e3c86279f4c22700753b
Nirmit Dudeja <nirmitdudeja4@gmail.com>
2026-03-06 20:58:14 +0530

    seconn

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,30 @@ app.use(
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://campus4.netlify.app"
+  );
+
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,POST,PUT,DELETE,OPTIONS"
+  );
+
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization"
+  );
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  next();
+});
+
 /* ================= DATABASE ================= */
 
 mongoose.connect(process.env.MONGO_URI)
